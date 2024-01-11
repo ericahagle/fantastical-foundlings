cd9fc8953ab21a7af490242f4c722f552703d466
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-11 13:27:47 -0800

    refactor: Gen eralize classes a bit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
       <Link className='app-title-link' reloadDocument to='/'>
         <h1 className='app-title font-face-modesto-condensed'>Fantastical Foundlings</h1>
       </Link>
-      <Link className='adoptable-creatures-nav-link'>
-        <button className='adoptable-creatures-nav-button font-face-modesto-expanded'>Adoptable Creatures</button>
+      <Link className='nav-link'>
+        <button className='nav-button font-face-modesto-expanded'>Adoptable Creatures</button>
       </Link>
     </header>
   );
