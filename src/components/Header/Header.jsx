54b39da490c58b0c4a9208d9cef187b5b23fa90f
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-11 11:23:20 -0800

    feat: Apply css styling to header; add button to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header>
-      <h1 className='app-title'>Fantastical Foundlings</h1>
-      
+      <Link className='app-title-link' reloadDocument to='/'>
+        <h1 className='app-title'>Fantastical Foundlings</h1>
+      </Link>
+      <Link className='adoptable-creatures-nav-link'>
+        <button className='adoptable-creatures-nav-button'>Adoptable Creatures</button>
+      </Link>
     </header>
   );
 };
