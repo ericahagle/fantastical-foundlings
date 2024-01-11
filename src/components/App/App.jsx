0d41046ea6e6b66c3c2e818ef623350754e229ac
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-11 10:01:23 -0800

    feat: Add Header component with title

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from '../Home/Home';
+import Header from '../Header/Header';
 
 const App = () => {
   return (
     <main className='App'>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='app-title'>Fantastical Foundlings</h1>
+      
+    </header>
+  );
+};
+
+export default Header;
