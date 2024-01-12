ab13d6325ee9bb84f74ee93fa9fd5c3cf81885a6
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-11 22:45:41 -0800

    refactor: Remove unused fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,3 @@
-// const getCreature = (index) => {
-//   return fetch(`https://www.dnd5eapi.co/api/monsters/${index}`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         if (response.status >= 400 && response.status < 500) {
-//           throw new Error("Sorry traveler! There's nothing to see here!");
-//         } else if (response.status >= 500) {
-//           throw new Error("Sorry traveler! Something went wrong. Please try again later.");
-//         } else {
-//           throw new Error(`Unexpected error. Status: ${response.status}`);
-//         }
-//       }
-//       return response.json();
-//     })
-// }
-
 const BASE_URL = 'https://www.dnd5eapi.co';
 
 const getAllCreatures = () => {
