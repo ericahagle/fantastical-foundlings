7937fb192b260cc80ad4c39f1e0d77b1b80a97d9
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-12 17:34:48 -0800

    feat: Add helper conditional functions to render elements only if they exist

--- src/components/CreatureDetail/CreatureDetail.jsx
+++ src/components/CreatureDetail/CreatureDetail.jsx
@@ -8,6 +8,32 @@ const CreatureDetail = () => {
   const [selectedCreature, setSelectedCreature] = useState(null);
   const [error, setError] = useState(null);
 
+  const renderElementIfExists = (label, value) => {
+    if (value) {
+      return (
+        <p>
+          <strong>{label} </strong>
+          {value}
+        </p>
+      );
+    };
+    return null;
+  };
+
+  const renderArrayIfExists = (label, array) => {
+    if (array && array.length > 0) {
+      return (
+        <p>
+          <strong>{label} </strong>
+          {array.map((item) => (
+            <p key={item.name}>{item.name}: {item.desc}</p>
+          ))}
+        </p>
+      );
+    };
+    return null;
+  };
+
   useEffect(() => {
     if (index) {
       getCreatureByIndex(index)
@@ -34,77 +60,24 @@ const CreatureDetail = () => {
           alt={selectedCreature.name}
         />
       )}
-      <p>
-        <strong>Size </strong>
-        {selectedCreature.size}
-      </p>
-      <p>
-        <strong>Type </strong>
-        {selectedCreature.type}
-      </p>
-      <p>
-        <strong>Alignment </strong>
-        {selectedCreature.alignment}
-      </p>
-      <p>
-        <strong>Armor Class </strong>
-        {selectedCreature.armor_class[0].value}
-      </p>
-      <p>
-        <strong>Walk Speed </strong>
-        {selectedCreature.speed.walk}
-        <strong> Fly Speed </strong>
-        {selectedCreature.speed.fly}
-        <strong> Swim Speed </strong>
-        {selectedCreature.speed.swim}
-      </p>
-      <p>
-        <strong>Hit Points </strong>
-        {selectedCreature.hit_points}
-      </p>
-      <p>
-        <strong>Strength </strong>
-        {selectedCreature.strength}
-      </p>
-      <p>
-        <strong>Dexterity </strong>
-        {selectedCreature.dexterity}
-      </p>
-      <p>
-        <strong>Constitution </strong>
-        {selectedCreature.constitution}
-      </p>
-      <p>
-        <strong>Intelligence </strong>
-        {selectedCreature.intelligence}
-      </p>
-      <p>
-        <strong>Wisdom </strong>
-        {selectedCreature.wisdom}
-      </p>
-      <p>
-        <strong>Charisma </strong>
-        {selectedCreature.charisma}
-      </p>
-      <p>
-        <strong>Languages </strong>
-        {selectedCreature.languages}
-      </p>
-      <p>
-        <strong>Special Abilities </strong>
-        {selectedCreature.special_abilities.map((ability) => 
-        (<p>{ability.name}: {ability.desc}</p>))}
-      </p>
-      <p>
-        <strong>Actions </strong>
-        {selectedCreature.actions.map((action) => 
-        (<p>{action.name}: {action.desc}</p>))}
-      </p>
-      <p>
-        <strong>Legendary Actions </strong>
-        {selectedCreature.legendary_actions.map((legendary_action) => 
-        (<p>{legendary_action.name}: {legendary_action.desc}</p>))}
-      </p>
+      {renderElementIfExists('Size', selectedCreature.size)}
+      {renderElementIfExists('Type', selectedCreature.type)}
+      {renderElementIfExists('Alignment', selectedCreature.alignment)}
+      {renderElementIfExists('Armor Class', selectedCreature.armor_class[0].value)}
+      {renderElementIfExists('Walk Speed', selectedCreature.speed.walk)}
+      {renderElementIfExists('Fly Speed', selectedCreature.speed.fly)}
+      {renderElementIfExists('Swim Speed', selectedCreature.speed.swim)}
+      {renderElementIfExists('Hit Points', selectedCreature.hit_points)}
+      {renderElementIfExists('Strength', selectedCreature.strength)}
+      {renderElementIfExists('Dexterity', selectedCreature.dexterity)}
+      {renderElementIfExists('Constitution', selectedCreature.constitution)}
+      {renderElementIfExists('Intelligence', selectedCreature.intelligence)}
+      {renderElementIfExists('Wisdom', selectedCreature.wisdom)}
+      {renderElementIfExists('Charisma', selectedCreature.charisma)}
+      {renderElementIfExists('Languages', selectedCreature.languages)}
+      {renderArrayIfExists('Special Abilities', selectedCreature.special_abilities)}
+      {renderArrayIfExists('Actions', selectedCreature.actions)}
+      {renderArrayIfExists('Legendary Actions', selectedCreature.legendary_actions)}
     </div>
   );
 };
