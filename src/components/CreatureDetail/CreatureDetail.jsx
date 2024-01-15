ae5bf1beaf84256c3b8736143bd8de74ddc33189
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-15 01:38:22 -0800

    feat: Update styling for Header and Creature Detail page

--- src/components/CreatureDetail/CreatureDetail.jsx
+++ src/components/CreatureDetail/CreatureDetail.jsx
@@ -12,8 +12,8 @@ const CreatureDetail = () => {
   const renderElementIfExists = (label, value) => {
     if (value) {
       return (
-        <p>
-          <strong>{label}: </strong>
+        <p className='paragraph-of-details'>
+          <strong className='detail-category'>{label}: </strong>
           {value}
         </p>
       );
@@ -24,12 +24,12 @@ const CreatureDetail = () => {
   const renderArrayIfExists = (label, array) => {
     if (array && array.length > 0) {
       return (
-        <div>
-          <p>
-            <strong>{label} </strong>
+        <div className='array-of-details'>
+          <p className='detail-array-items'>
+            <strong className='detail-category'>{label} </strong>
           </p>
           {array.map((item) => (
-            <p key={item.name}>{item.name}: {item.desc}</p>
+            <p className='detail-description' key={item.name}>{item.name}: {item.desc}</p>
           ))}
         </div>
       );
@@ -62,33 +62,41 @@ const CreatureDetail = () => {
     <div className='CreatureDetail'>
       {loading && <span className='loading'>Loading...</span>}
       {!loading && (
-        <>
-          <h2 className='creature-name'>{selectedCreature.name}</h2>
-          {selectedCreature.image && (
-            <img className='selected-creature-image'
-              src={`https://www.dnd5eapi.co${selectedCreature.image}`}
-              alt={selectedCreature.name}
-            />
-          )}
-          {renderElementIfExists('Size', selectedCreature.size)}
-          {renderElementIfExists('Type', selectedCreature.type)}
-          {renderElementIfExists('Alignment', selectedCreature.alignment)}
-          {renderElementIfExists('Armor Class', selectedCreature.armor_class[0].value)}
-          {renderElementIfExists('Walk Speed', selectedCreature.speed.walk)}
-          {renderElementIfExists('Fly Speed', selectedCreature.speed.fly)}
-          {renderElementIfExists('Swim Speed', selectedCreature.speed.swim)}
-          {renderElementIfExists('Hit Points', selectedCreature.hit_points)}
-          {renderElementIfExists('Strength', selectedCreature.strength)}
-          {renderElementIfExists('Dexterity', selectedCreature.dexterity)}
-          {renderElementIfExists('Constitution', selectedCreature.constitution)}
-          {renderElementIfExists('Intelligence', selectedCreature.intelligence)}
-          {renderElementIfExists('Wisdom', selectedCreature.wisdom)}
-          {renderElementIfExists('Charisma', selectedCreature.charisma)}
-          {renderElementIfExists('Languages', selectedCreature.languages)}
-          {renderArrayIfExists('Special Abilities', selectedCreature.special_abilities)}
-          {renderArrayIfExists('Actions', selectedCreature.actions)}
-          {renderArrayIfExists('Legendary Actions', selectedCreature.legendary_actions)}
-        </>
+        <div className='creature-detail-container'>
+          <div className='left-side'>
+            <h2 className='creature-name'>{selectedCreature.name}</h2>
+            {selectedCreature.image && (
+              <img className='selected-creature-image'
+                src={`https://www.dnd5eapi.co${selectedCreature.image}`}
+                alt={selectedCreature.name}
+              />
+            )}
+          </div>
+
+          <div className='right-side'>
+            {renderElementIfExists('Size', selectedCreature.size)}
+            {renderElementIfExists('Type', selectedCreature.type)}
+            {renderElementIfExists('Alignment', selectedCreature.alignment)}
+            {renderElementIfExists('Armor Class', selectedCreature.armor_class[0].value)}
+            {renderElementIfExists('Walk Speed', selectedCreature.speed.walk)}
+            {renderElementIfExists('Fly Speed', selectedCreature.speed.fly)}
+            {renderElementIfExists('Swim Speed', selectedCreature.speed.swim)}
+            {renderElementIfExists('Hit Points', selectedCreature.hit_points)}
+            {renderElementIfExists('Strength', selectedCreature.strength)}
+            {renderElementIfExists('Dexterity', selectedCreature.dexterity)}
+            {renderElementIfExists('Constitution', selectedCreature.constitution)}
+            {renderElementIfExists('Intelligence', selectedCreature.intelligence)}
+            {renderElementIfExists('Wisdom', selectedCreature.wisdom)}
+            {renderElementIfExists('Charisma', selectedCreature.charisma)}
+            {renderElementIfExists('Languages', selectedCreature.languages)}
+          </div>
+
+          <div className='full-width'>
+            {renderArrayIfExists('Special Abilities', selectedCreature.special_abilities)}
+            {renderArrayIfExists('Actions', selectedCreature.actions)}
+            {renderArrayIfExists('Legendary Actions', selectedCreature.legendary_actions)}
+          </div>
+        </div>
       )}
     </div>
   );
