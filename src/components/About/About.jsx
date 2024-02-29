698807ca8bc2c5420fa1b2dee6b2e98c83968104
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-28 21:08:46 -0800

    feat: Add 'About Us' copy and header for 'Meet the Staff'

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,16 @@ import './About.css';
 const About = () => {
   return (
     <div className='About'>
-      <h2>About Us</h2>
+      <h2 className='page-title font-face-modesto-expanded'>About Us</h2>
+      <div className='main-page-copy font-face-bookman-bt-roman-headline'>
+        <p>
+          Created by a traveling party who decided to stop searching to fill that necrotic hole in our soul with the blood of others, we had enough coin to buy a lot of farmland on the buffer zone between the adversaries of the Eastern war, but quickly discovered that none of us knew how to farm or were ever going to be very good at it.
+        </p>
+        <p>
+          We love creatures of all sizes and alignments, and immediately began to pick up strays. Once we started accumulating more than we could handle, we needed to find them new homes. A few months of this led to spreading word of mouth and strangers showing up at our front door asking if we have any Displacer Beasts. (We get it, they're cool, but nobody—I repeat, nobody—has Displacer Beasts year-round, and when we do, they go fast.)
+        </p>
+        <h2 className='page-title font-face-modesto-expanded'>Meet the Staff!</h2>
+      </div>
     </div>
   );
 };
